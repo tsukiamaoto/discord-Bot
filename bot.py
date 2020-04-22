2e7ab1fc245dfd7e1a3793836cfbd788cbb84c8b
gary <gomrdhl796@gmail.com>
2020-04-22 17:17:31 +0800

    initial setting

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='[')
+
+@bot.event
+async def on_ready():
+  print("Bot is online")
+
+@bot.event
+async def on_member_join(member):
+  channel = bot.get_channel(702443826916229160)
+  await channel.send(f'{member} join!')
+
+@bot.event
+async def on_member_remove(member):
+  channel = bot.get_channel(702443826916229160)
+  await channel.send(f'{member} leave!')
+
+bot.run('NzAyNDM1OTgxOTYxMzMwNzQ5.XqABaA.JjoEHmcnFLEWB_AwAGw3Mpf_Ktc')
